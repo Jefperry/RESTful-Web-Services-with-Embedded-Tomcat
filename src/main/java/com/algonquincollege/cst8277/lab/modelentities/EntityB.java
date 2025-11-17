3a44c62cf23348f90bd70182143fc1f2e8c720dd
jefperry89 <jefperry89@gmail.com>
2025-11-16 23:12:37 -0500

    Implement REST endpoints for GoodbyeWorld, HelloWorld, and ManagedRef resources; add logging configuration for logback; include test cases for entity systems.

---
+++ src/main/java/com/algonquincollege/cst8277/lab/modelentities/EntityB.java
@@ -0,0 +1,18 @@
+package com.algonquincollege.cst8277.lab.modelentities;
+
+import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@SuppressWarnings("unused")
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class EntityB extends PojoBase implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	public EntityB() {
+		super();
+		//Hard-code a message
+		setFoobar("bee");
+	}
+}
